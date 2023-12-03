1b09cab05aa2ac2e47563a800bfd1037aa998384
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-12-03 19:15:01 +0800

    tinkertool 9.65

---
+++ Casks/tinkertool.rb
@@ -0,0 +1,22 @@
+cask "tinkertool" do
+  version "9.65"
+  sha256 "6062f38b17b206121d18cf33647782bd0d2cf9d669a6205a9cbcd37445c3e162"
+
+  url "https://www.bresink.eu/download3.php?PHPSESSID=",
+      verified: "www.bresink.biz"
+  name "TinkerTool"
+  desc "macOS 系统配置小工具"
+  homepage "https://www.bresink.com/osx/TinkerTool.html"
+
+  livecheck do
+    url :homepage
+    regex(/(\d+(\.\d+)+).+released/i)
+  end
+
+  app "TinkerTool.app"
+
+  zap trash: [
+    "~/Library/Containers/com.bresink.system.tinkertool",
+    "~/Library/Application Scripts/com.bresink.system.tinkertool",
+  ]
+end
