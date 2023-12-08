f6383c00c8de95d5fbc6057bb7ede28eb5721f27
ivaquero <msaintms@outlook.com>
2023-12-08 20:51:02 +0800

    clash-verge-rev: update link

--- Casks/clash-verge-rev.rb
+++ Casks/clash-verge-rev.rb
@@ -5,10 +5,10 @@ cask "clash-verge-rev" do
   sha256 arm:   "8c8175b10c1db2612d542ea0aee7bc6a12377817f6d0c1ecbfd2e64e18f02ad8",
          intel: "da0248a754b33e7641e0d96964d789d71503540e8b760f5d623600b398866945"
 
-  url "https://github.com/wonfen/clash-verge-rev/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
+  url "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
   name "Clash Verge Rev"
   desc "Clash Verge 接盘项目"
-  homepage "https://github.com/wonfen/clash-verge-rev"
+  homepage "https://github.com/clash-verge-rev/clash-verge-rev"
 
   livecheck do
     url :url
