de04709c0c7535f7c1ea662eb9c87c6c412af753
ivaquero <msaintms@outlook.com>
2023-12-12 20:10:26 +0800

    openai-translator 0.2.14

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -1,9 +1,9 @@
 cask "openai-translator" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.2.5"
-  sha256 arm:   "05e60021a35a5fa83dd2d3e683f4580dbadc0338336bd0fb1b14bce39d46256d",
-         intel: "f8bb2149f13a0c6f8bdcb1a391cc0c584a02506db6b44498004fbaccde3a1c84"
+  version "0.2.14"
+  sha256 arm:   "fdae4dbc55ce4e74275d6b6cc760bbb59d9e73ed2b9ba7d9f43b4de018d16a44",
+         intel: "847b55cebab6c26ba47a030cfa648d1821e6643e9ab1778d4413cb11caaf459f"
 
   url "https://github.com/openai-translator/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_#{arch}.dmg"
   name "OpenAI Translator"
