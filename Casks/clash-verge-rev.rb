6d21c87080ad4549273c35fc1ca24f0ee792fcba
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-12-15 17:48:17 +0800

    clash-verge-rev 1.4.5

--- Casks/clash-verge-rev.rb
+++ Casks/clash-verge-rev.rb
@@ -2,8 +2,8 @@ cask "clash-verge-rev" do
   arch arm: "aarch64", intel: "x64"
 
   version "1.4.5"
-  sha256 arm:   "9ac1de52324088dd1b07b8af00bec314a445da44b587d34e89255104f43b4740",
-         intel: "ea925f0e2a338e6584ebe735f8c452254941d8e03135e7b830f38f4554c6582d"
+  sha256 arm:   "30c58b3297f407ea3296a0e05f45e263bec98ed695e6b859244df2ebf60f93b1",
+         intel: "841d8f6d5c5b7e997636c51adaeee8a48a64fa22a03a3da064bb885578a01bff"
 
   url "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
   name "Clash Verge Rev"
