60796b7b7763b18d7dc4540faa82795980bafc04
runner <runner@Mac-1703312141801.local>
2023-12-23 06:19:53 +0000

    pot-desktop 2.7.3

--- Casks/pot-desktop.rb
+++ Casks/pot-desktop.rb
@@ -1,9 +1,9 @@
 cask "pot-desktop" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2.7.2"
-  sha256 arm:   "43139a063d2d49341cab83555e38e43dd0346b3e4426bbdef49cd945044ec408",
-         intel: "4e0d4799e62ae432770650834c099bd448d1d6125c058ef8dacbd5d7f39860a7"
+  version "2.7.3"
+  sha256 arm:   "091a5e7fd73a5bc97ce31151be36acf86a8bd2c793ce7eb6f9c71f69cdf0e5c5",
+         intel: "709a631b746327a811229b1734f1ed6a6b341f25c9fb7afc0002b09c9ff55672"
 
   mirror = "https://mirror.ghproxy.com/"
   origin = "https://github.com/pot-app/pot-desktop/releases/download/#{version}/pot_#{version}_#{arch}.dmg"
