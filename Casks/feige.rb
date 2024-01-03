593d1a778ae5e39be1760574da59bfe4cd3f09c2
ivaquero <msaintms@outlook.com>
2024-01-03 21:37:58 +0800

    feige 7.1.20240101

--- Casks/feige.rb
+++ Casks/feige.rb
@@ -1,18 +1,18 @@
 cask "feige" do
-  version "2.0.220328"
+  version "7.1.20240101"
   sha256 "bd95bad5c8a31cbaf82fa8e284e2eb719f8963866a81b61ab2f2021d051de878"
 
-  url "http://www.ipmsg.org.cn/Download/Feige_Mac.dmg"
+  url "http://www.ipmsg.org.cn/Download/Feige_Mac.dmg?#{version}"
   name "Feige"
   desc "面向企业、学校、家庭的局域网即时通讯软件"
   homepage "http://www.ipmsg.org.cn/"
 
-  auto_updates true
-
   livecheck do
-    url 'http://www.ipmsg.org.cn/home/index/download'
-    regex(/Mac版<\/h3>[^<]*<i>([^<]+)<\/i>/i)
+    url "http://www.ipmsg.org.cn/home/index/download"
+    regex(%r{Mac版</h3>[^<]*<i>([^<]+)</i>}i)
   end
+
+  auto_updates true
 
   app "Feige.app"
 
