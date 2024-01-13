411e9acf5a329acfcf9a3c7e5c5dc3078746007a
Oscaner Miao <oscaner1997@gmail.com>
2024-01-13 21:31:32 +0800

    fix: fix freecad-cn url

--- Casks/freecad-cn.rb
+++ Casks/freecad-cn.rb
@@ -1,11 +1,11 @@
 cask "freecad-cn" do
-  arch arm: "arm64", intel: "intel_x86"
+  arch arm: "arm64", intel: "intel-x86_64"
 
   version "0.21.1"
   sha256 arm:   "b6959ca9e0e2f7cddda2cf1e97a26f3e2e65205f2e8e53b3c5dccd062f0be14f",
          intel: "633dd754e7732c531a019fe74068e43883f8f329c25fe85bfcc91fa26186451f"
 
-  url "https://mirrors.tuna.tsinghua.edu.cn/github-release/FreeCAD/FreeCAD/#{version}/FreeCAD-#{version}-mac-#{arch}.dmg",
+  url "https://mirrors.tuna.tsinghua.edu.cn/github-release/FreeCAD/FreeCAD/FreeCAD%20#{version}/FreeCAD-#{version}-macOS-#{arch}.dmg",
       verified: "mirrors.tuna.tsinghua.edu.cn/github-release/FreeCAD/FreeCAD/"
   name "FreeCAD"
   desc "3D parametric modeler"
