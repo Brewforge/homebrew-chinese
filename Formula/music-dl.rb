a14dd85846646ea9ddc58e3e7f6c2d6046b4ed32
Oscaner Miao <oscaner1997@gmail.com>
2024-02-23 12:14:49 +0800

    fix: url of music-dl

--- Formula/music-dl.rb
+++ Formula/music-dl.rb
@@ -4,7 +4,7 @@ class MusicDl < Formula
   license "MIT"
   version "4.1.7"
 
-  url "https://github.com/guanguans/music-dl/releases/download/4.1.6/music-dl.phar"
+  url "https://github.com/guanguans/music-dl/releases/download/4.1.7/music-dl.phar"
   sha256 "50155de55d2382b2b8214d0601f1d532c7f529b955d8d72abdf92d49a15b1e24"
 
   depends_on "php"
