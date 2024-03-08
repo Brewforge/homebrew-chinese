3337b75e424077de0de98415ef8ae02479114d0a
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2024-03-08 14:11:28 +0800

    messauto 0.6.4

--- Casks/messauto.rb
+++ Casks/messauto.rb
@@ -1,9 +1,9 @@
 cask "messauto" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.6.3"
-  sha256 arm:   "426d3f2682e26d10a85331f11f9309b4282f4ab4d679341630bcd756522b91a7",
-         intel: "ab25571a97914715ea5f209e4782067aff3d50355ba9a93c403c0a6b15df8fd5"
+  version "0.6.4"
+  sha256 arm:   "8ff3f4352f064ad29783d74842d1f6a2dbc347109447ce5793c69d8a42a0db49",
+         intel: "9441a0dbe24802c3ac28064626b3fcb29519eaec3eb1094b48fd4d972cba1f95"
 
   mirror = "https://mirror.ghproxy.com/"
   origin = "https://github.com/LeeeSe/MessAuto/releases/download/v#{version}/MessAuto_#{arch}.zip"
