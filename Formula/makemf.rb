f021cd4d2f50672fc0113dc681cdcf81a6779a86
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2024-04-29 22:21:41 +0800

    makemf 0.0.28 ollamaplist 0.0.28

--- Formula/makemf.rb
+++ Formula/makemf.rb
@@ -1,10 +1,10 @@
 class Makemf < Formula
   desc "让 GGUF 在 Ollama 中运行"
   homepage "https://github.com/Mrered/ShellScript"
-  version "0.0.18"
+  version "0.0.28"
 
   url "https://github.com/Mrered/ShellScript/releases/download/v#{version}/makemf_v#{version}.tar.gz"
-  sha256 "1c80e09bd0cee70c0072b8f19b5933be4e0c9c18abba617479e5aea71c2e66c3"
+  sha256 "1bba690c753fd92399baa51624a2ded74d8319180021300e372e2863c42728ee"
 
   livecheck do
     url "https://github.com/Mrered/ShellScript/releases"
@@ -13,5 +13,9 @@ class Makemf < Formula
 
   def install
     bin.install "makemf"
+  end
+
+  def post_install
+    chmod 0555, bin/"makemf"
   end
 end
--- Formula/ollamaplist.rb
+++ Formula/ollamaplist.rb
@@ -1,10 +1,10 @@
 class Ollamaplist < Formula
   desc "让 Ollama 监听 0.0.0.0"
   homepage "https://github.com/Mrered/ShellScript"
-  version "0.0.18"
+  version "0.0.28"
 
   url "https://github.com/Mrered/ShellScript/releases/download/v#{version}/ollamaplist_v#{version}.tar.gz"
-  sha256 "a12b0d11745fc900b0a208b069c7b91a7cdd42f559a1717b9b71f74fb6d173c5"
+  sha256 "a7212ca4fa68091d15f8d0b516a8da00d0e2dd2a78983fa173f9246d32b665f0"
 
   livecheck do
     url "https://github.com/Mrered/ShellScript/releases"
@@ -13,5 +13,9 @@ class Ollamaplist < Formula
 
   def install
     bin.install "ollamaplist"
+  end
+
+  def post_install
+    chmod 0555, bin/"ollamaplist"
   end
 end
