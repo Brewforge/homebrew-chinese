3e899225c593183c718976b10d0d82ac5b2cf94c
ivaquero <msaintms@outlook.com>
2024-05-06 19:16:38 +0800

    openai-translator 0.4.8

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -1,6 +1,6 @@
 cask "openai-translator" do
-  version "0.4.6"
-  sha256 "bd1345a1129ce8ef2f3705a818f9c3bb0c97c1949ebec430e6b6a73b2bc872ba"
+  version "0.4.8"
+  sha256 "c40ceaa220dbcfee3f21e2e70608d89947f43d0e20599888350c5d9688cfe6d6"
 
   mirror = "https://mirror.ghproxy.com/"
   origin = "https://github.com/openai-translator/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_aarch64.dmg"
