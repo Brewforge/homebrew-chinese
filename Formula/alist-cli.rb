048059644c8d017daf74fa26f98c348903ce8028
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2024-05-08 02:29:01 +0800

    修复 alist-cli 导致的 CI 失败

--- Formula/alist-cli.rb
+++ Formula/alist-cli.rb
@@ -7,16 +7,16 @@ class AlistCli < Formula
   mirror = "https://mirror.ghproxy.com/"
   if OS.mac? && Hardware::CPU.arm?
     origin = "https://github.com/alist-org/alist/releases/download/v#{version}/alist-darwin-arm64.tar.gz"
-    sha256 "78b6d3071b6ef8afbb8d2c3c4fdc88627f1e0e1fa61e76675fd4731596c2c965"
+    sha256 "59b7b3737fdd066858a4cd6cfd527516e1feac3b9074f4ecb7b5ff759baf4155"
   elsif OS.mac? && !Hardware::CPU.arm?
     origin = "https://github.com/alist-org/alist/releases/download/v#{version}/alist-darwin-amd64.tar.gz"
     sha256 "39d43c1f3c70510075e9e9e3e159a647d25bd35461b1c5582a6d3ef88cb51a5f"
   elsif !OS.mac? && Hardware::CPU.arm?
     origin = "https://github.com/alist-org/alist/releases/download/v#{version}/alist-linux-arm64.tar.gz"
-    sha256 "606a6bcff1abd25bd5aaaf9663b62bfd3281e575dda9df1852b2e48f23511cae"
+    sha256 "1cb865edb99452dfd0fad35c4c4dbee45f9756259c2efda837fa5fdf7d7782f0"
   elsif !OS.mac? && !Hardware::CPU.arm?
     origin = "https://github.com/alist-org/alist/releases/download/v#{version}/alist-linux-amd64.tar.gz"
-    sha256 "81f66e521fe48cb18daadab2d175116201d3ef6b5d5d7bbac1835493eb6e1c81"
+    sha256 "24c034739f241783d844c1b14f4655e1b88d1cca5009c0df7f9de5c366522b4a"
   end
   url "#{mirror}#{origin}",
       verified: "mirror.ghproxy.com/"
