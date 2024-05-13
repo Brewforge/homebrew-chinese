5991816f528a1e3f72c78b2cf0a9cdd87f4629cf
ivaquero <msaintms@outlook.com>
2024-05-13 19:41:47 +0800

    eshelper: use latest

--- Casks/eshelper.rb
+++ Casks/eshelper.rb
@@ -1,18 +1,18 @@
 cask "eshelper" do
-  version "4.6.4"
-  sha256 "aa8661267067eaa3fdbc5a61e0326e61ce88426256136a41abab4c97e09c4e8e"
+  version :latest
+  sha256 :no_check
 
-  url "https://static.eudic.net/pkg/ehmac.dmg?v=#{version}",
+  url "https://static.eudic.net/pkg/ehmac.dmg",
       user_agent: :fake
   name "Eshelper"
   name "西语助手"
   desc "Spanish Chinese dictionary and learning tool"
   homepage "https://www.eudic.net/v4/es/app/eshelper"
 
-  livecheck do
-    url "https://static.eudic.net/pkg/eshelper_mac.xml?noCache=1"
-    strategy :sparkle, &:short_version
-  end
+  # livecheck do
+  #   url "https://static.eudic.net/pkg/eshelper_mac.xml?noCache=1"
+  #   strategy :sparkle, &:short_version
+  # end
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
