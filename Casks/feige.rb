3bd175a9cff27e228f1015c7e6433806bcc98c16
ivaquero <msaintms@outlook.com>
2024-05-16 10:15:17 +0800

    feige: fix url

--- Casks/feige.rb
+++ Casks/feige.rb
@@ -1,8 +1,8 @@
 cask "feige" do
-  version "7.1.20240101"
+  version "7.1"
   sha256 "bd95bad5c8a31cbaf82fa8e284e2eb719f8963866a81b61ab2f2021d051de878"
 
-  url "http://www.ipmsg.org.cn/Download/Feige_Mac.dmg?#{version}"
+  url "http://www.ipmsg.org.cn/Download/Feige-#{version}-x86_64.dmg"
   name "Feige"
   desc "面向企业、学校、家庭的局域网即时通讯软件"
   homepage "http://www.ipmsg.org.cn/"
