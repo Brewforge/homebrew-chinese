d9c81cabb00b0e4d724b2c8fa8a0fe68e38ce50d
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2024-05-26 11:00:26 +0800

    ugreennas 更新 lc

--- Casks/ugreennas.rb
+++ Casks/ugreennas.rb
@@ -9,7 +9,7 @@ cask "ugreennas" do
 
   livecheck do
     url "https://www.ugnas.com/download"
-    regex(/href="[^"]*appVer\/macOS\/[^"]*?(\d+\.\d+\.\d+\.\d+-[a-zA-Z0-9]+)/)
+    regex(/href="[^"]*\/macOS\/[^"]*?(\d+\.\d+\.\d+\.\d+-[a-zA-Z0-9]+)/i)
   end
 
   auto_updates true
