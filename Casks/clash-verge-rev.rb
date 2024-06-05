b076f7caa346007a5c7451093075b17cda88f5b9
Aki Akise <akiakise@outlook.com>
2024-06-05 10:07:01 +0800

    clash-verge-rev: fix intel hash

--- Casks/clash-verge-rev.rb
+++ Casks/clash-verge-rev.rb
@@ -3,7 +3,7 @@ cask "clash-verge-rev" do
 
   version "1.6.5"
   sha256 arm:   "911a1797d2762fb59e75d35a9798cd69ad1749d6200898d55adbc5deef1d271e",
-         intel: "98642ee60bb6b9e2944432d333a22598f2cff8ed1cf67d1cb5d87c94b94cd299"
+         intel: "6810c7f697c6ccbd6d38ac2ac3194d759c1ba97041cb217b6dfa8470af3ae069"
 
   mirror = "https://mirror.ghproxy.com/"
   origin = "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
