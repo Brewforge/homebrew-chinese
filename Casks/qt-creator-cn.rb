f74b17717556731dabe3949f15f229f76373b778
ivaquero <msaintms@outlook.com>
2024-06-16 15:18:55 +0800

    qt-creator-cn: fix livecheck

--- Casks/qt-creator-cn.rb
+++ Casks/qt-creator-cn.rb
@@ -8,24 +8,9 @@ cask "qt-creator-cn" do
   desc "IDE for application development"
   homepage "https://www.qt.io/developers/"
 
-  # It's necessary to check within a major/minor version directory
-  # (fetching an additional page) to obtain the full version.
   livecheck do
-    url "https://download.qt.io/official_releases/qtcreator/?C=M;O=D"
-    regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
-    strategy :page_match do |page, regex|
-      # These version directories can sometimes be empty, so this will check
-      # directory pages until it finds versions
-      page.scan(regex).lazy.map do |match|
-        version_page = Homebrew::Livecheck::Strategy.page_content(url.sub("/?", "/#{match[0]}/?"))
-        next if version_page[:content].blank?
-
-        versions = version_page[:content].scan(regex).map(&:first)
-        next if versions.blank?
-
-        versions
-      end.compact_blank.first
-    end
+    url "https://mirrors.tuna.tsinghua.edu.cn/qt/official_releases/qtcreator/latest/"
+    regex(/qt-creator-opensource-mac-x86_64-(.\d+(\.\d+)+)\.dmg/i)
   end
 
   auto_updates true
