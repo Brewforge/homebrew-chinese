d08dc9c3c574d0232fa7d3e05df0ae0939558c41
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2024-06-23 22:42:43 +0800

    ollamaplist 0.1.0

--- Formula/ollamaplist.rb
+++ Formula/ollamaplist.rb
@@ -1,10 +1,25 @@
 class Ollamaplist < Formula
-  desc "让 Ollama 监听 0.0.0.0"
-  homepage "https://github.com/Mrered/ShellScript"
-  version "0.0.30"
+  desc "给 Ollama CLI 添加环境变量"
+  homepage "https://github.com/Mrered/Gobin"
+  version "0.1.0"
 
-  url "https://github.com/Mrered/ShellScript/releases/download/v#{version}/ollamaplist_v#{version}.tar.gz"
-  sha256 "85747c5b6a8d13f3c90a569661c85e2e7457d337fb1e7b223be0d50f7d4e9565"
+  arch = Hardware::CPU.arm? ? "arm64" : "amd64"
+
+  if OS.mac?
+    url "https://github.com/Mrered/Gobin/releases/download/v#{version}/Gobin_#{version}_darwin_#{arch}.tar.gz"
+    if Hardware::CPU.arm?
+      sha256 "8b1f4a6a7e140b8c5284d85fd4ff6a58b0b7a9ad18dbb4cbb27c5b5386bc85b9"
+    else
+      sha256 "9ea2e0fb021143c8354dbe8e2ca63b960d45b2d270e24ca876d8c0ce86ac3162"
+    end
+  else
+    url "https://github.com/Mrered/Gobin/releases/download/v#{version}/Gobin_#{version}_linux_#{arch}.tar.gz"
+    if Hardware::CPU.arm?
+      sha256 "c1b53a1c4402a92ce847cb8056fca8f8f577383fbec155474ef1ddf26f15914f"
+    else
+      sha256 "2d3f5b48fa913e2b3aa2f2be44eede5dc8a213735ff361ab5b23176428730508"
+    end
+  end
 
   livecheck do
     url "https://github.com/Mrered/ShellScript/releases"
