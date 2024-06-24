59455a9039fd3f09124bba40b2a09f4a8ed8215f
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2024-06-24 18:50:39 +0800

    makemf 0.2.2 ollamaplist 0.2.2

--- Formula/makemf.rb
+++ Formula/makemf.rb
@@ -1,13 +1,28 @@
 class Makemf < Formula
   desc "让 GGUF 在 Ollama 中运行"
-  homepage "https://github.com/Mrered/ShellScript"
-  version "0.0.31"
+  homepage "https://github.com/Mrered/Gobin"
+  version "0.2.2"
 
-  url "https://github.com/Mrered/ShellScript/releases/download/v#{version}/makemf_v#{version}.tar.gz"
-  sha256 "85f68ecc7192d51afcecd8260d6af1157fe8968ae08c63e62f7e956747559405"
+  arch = Hardware::CPU.arm? ? "arm64" : "amd64"
+
+  if OS.mac?
+    url "https://github.com/Mrered/Gobin/releases/download/v#{version}/makemf_#{version}_darwin_#{arch}.tar.gz"
+    if Hardware::CPU.arm?
+      sha256 "d1a4fdd94b56253f76d572cede00dba7e17ebe4bb6d4e0bd2b2d28baa2cda20f"
+    else
+      sha256 "a9a4a1e0ae92d996a7db3da6d4bb84bce689963b1d1083e0f26569ad939013d9"
+    end
+  else
+    url "https://github.com/Mrered/Gobin/releases/download/v#{version}/makemf_#{version}_linux_#{arch}.tar.gz"
+    if Hardware::CPU.arm?
+      sha256 "e75a5fa805f2cd383f7f1ae0a6f8065eb38fdf41d59607d44abd675a9419a23d"
+    else
+      sha256 "56a40362b8c8b5b47238932a76dc880d3a7a749c0b1c6626f66549dd5a27381f"
+    end
+  end
 
   livecheck do
-    url "https://github.com/Mrered/ShellScript/releases"
+    url "https://github.com/Mrered/Gobin/releases"
     strategy :github_latest
   end
 
--- Formula/ollamaplist.rb
+++ Formula/ollamaplist.rb
@@ -1,28 +1,21 @@
 class Ollamaplist < Formula
   desc "给 Ollama CLI 添加环境变量"
   homepage "https://github.com/Mrered/Gobin"
-  version "0.1.0"
+  version "0.2.2"
 
   arch = Hardware::CPU.arm? ? "arm64" : "amd64"
 
   if OS.mac?
-    url "https://github.com/Mrered/Gobin/releases/download/v#{version}/Gobin_#{version}_darwin_#{arch}.tar.gz"
+    url "https://github.com/Mrered/Gobin/releases/download/v#{version}/ollamaplist_#{version}_darwin_#{arch}.tar.gz"
     if Hardware::CPU.arm?
-      sha256 "8b1f4a6a7e140b8c5284d85fd4ff6a58b0b7a9ad18dbb4cbb27c5b5386bc85b9"
+      sha256 "9ffa61dfdc430d21c7e675205a3a73768f9a063065e8b8f9a55b3b3b71d24ebf"
     else
-      sha256 "9ea2e0fb021143c8354dbe8e2ca63b960d45b2d270e24ca876d8c0ce86ac3162"
-    end
-  else
-    url "https://github.com/Mrered/Gobin/releases/download/v#{version}/Gobin_#{version}_linux_#{arch}.tar.gz"
-    if Hardware::CPU.arm?
-      sha256 "c1b53a1c4402a92ce847cb8056fca8f8f577383fbec155474ef1ddf26f15914f"
-    else
-      sha256 "2d3f5b48fa913e2b3aa2f2be44eede5dc8a213735ff361ab5b23176428730508"
+      sha256 "d311991876899604b5000730e21f0c17c38d34fef4a5d2e6f00ade044250d008"
     end
   end
 
   livecheck do
-    url "https://github.com/Mrered/ShellScript/releases"
+    url "https://github.com/Mrered/Gobin/releases"
     strategy :github_latest
   end
 
