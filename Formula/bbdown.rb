b3bf9330d64da9dc86e6e869a4c408303222d3d0
ivaquero <msaintms@outlook.com>
2024-09-21 19:06:52 +0800

    formulae: fix livecheck

--- Formula/bbdown.rb
+++ Formula/bbdown.rb
@@ -1,25 +1,23 @@
 class Bbdown < Formula
   desc "Bilibili Downloader"
   homepage "https://github.com/nilaoda/BBDown"
-
-  version "1.6.2,20240512"
   license "MIT"
 
   mirror = "https://mirror.ghproxy.com/"
   if OS.mac? && Hardware::CPU.arm?
-    url "#{mirror}https://github.com/nilaoda/BBDown/releases/download/#{version.to_s.split(",")[0]}/BBDown_#{version.to_s.tr(",", "_")}_osx-arm64.zip",
+    url "#{mirror}https://github.com/nilaoda/BBDown/releases/download/1.6.2/BBDown_1.6.2_20240512_osx-arm64.zip",
       verified: "mirror.ghproxy.com/"
     sha256 "65e81ecdf574c2ee8d2d7c36afcc17a67c53e0172acc6f8f642d89da698ed44d"
   elsif OS.mac? && !Hardware::CPU.arm?
-    url "#{mirror}https://github.com/nilaoda/BBDown/releases/download/#{version.to_s.split(",")[0]}/BBDown_#{version.to_s.tr(",", "_")}_osx-x64.zip",
+    url "#{mirror}https://github.com/nilaoda/BBDown/releases/download/1.6.2/BBDown_1.6.2_20240512_osx-x64.zip",
       verified: "mirror.ghproxy.com/"
     sha256 "69e8f1affbbd479847a0d169e23ed4be8051d1627e0067dd9fc1711f72401a6a"
   elsif !OS.mac? && Hardware::CPU.arm?
-    url "#{mirror}https://github.com/nilaoda/BBDown/releases/download/#{version.to_s.split(",")[0]}/BBDown_#{version.to_s.tr(",", "_")}_linux-arm64.zip",
+    url "#{mirror}https://github.com/nilaoda/BBDown/releases/download/1.6.2/BBDown_1.6.2_20240512_linux-arm64.zip",
       verified: "mirror.ghproxy.com/"
     sha256 "5bb1ab8987a33be08ad07367d2e70ea4f18868ba4538a77019aba48e76ed7892"
   elsif !OS.mac? && !Hardware::CPU.arm?
-    url "#{mirror}https://github.com/nilaoda/BBDown/releases/download/#{version.to_s.split(",")[0]}/BBDown_#{version.to_s.tr(",", "_")}_linux-x64.zip",
+    url "#{mirror}https://github.com/nilaoda/BBDown/releases/download/1.6.2/BBDown_1.6.2_20240512_linux-x64.zip",
       verified: "mirror.ghproxy.com/"
     sha256 "69e8f1affbbd479847a0d169e23ed4be8051d1627e0067dd9fc1711f72401a6a"
   end
--- Formula/ollamaplist.rb
+++ Formula/ollamaplist.rb
@@ -1,17 +1,15 @@
 class Ollamaplist < Formula
   desc "给 Ollama CLI 添加环境变量"
   homepage "https://github.com/Mrered/Gobin"
-
-  version "0.2.2"
   license "MIT"
 
   mirror = "https://mirror.ghproxy.com/"
   if OS.mac? && Hardware::CPU.arm?
-    url "#{mirror}https://github.com/Mrered/Gobin/releases/download/v#{version}/ollamaplist_#{version}_darwin_arm64.tar.gz",
+    url "#{mirror}https://github.com/Mrered/Gobin/releases/download/v0.2.2/ollamaplist_0.2.2_darwin_arm64.tar.gz",
       verified: "mirror.ghproxy.com/"
     sha256 "9ffa61dfdc430d21c7e675205a3a73768f9a063065e8b8f9a55b3b3b71d24ebf"
   elsif OS.mac? && !Hardware::CPU.arm?
-    url "#{mirror}https://github.com/Mrered/Gobin/releases/download/v#{version}/ollamaplist_#{version}_darwin_amd64.tar.gz",
+    url "#{mirror}https://github.com/Mrered/Gobin/releases/download/v0.2.2/ollamaplist_0.2.2_darwin_amd64.tar.gz",
       verified: "mirror.ghproxy.com/"
     sha256 "d311991876899604b5000730e21f0c17c38d34fef4a5d2e6f00ade044250d008"
   end
