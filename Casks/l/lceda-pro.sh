e256754783c5b8617a3f41bc57e0c2af5ea5881b
cnderrick <cnderrick@users.noreply.github.com>
2024-09-26 08:56:11 +0000

    finish install

--- Casks/l/lceda-pro.sh
+++ Casks/l/lceda-pro.sh
@@ -4,7 +4,8 @@ URL=$1
 STAGED_FILE=$2
 APP_DIR=$3
 
-# TODO: download lceda-pro from URL to STAGED_FILE, show progress
+# download lceda-pro from URL to STAGED_FILE, show progress
 curl -L -o $STAGED_FILE $URL
 
-# TODO: UNZIP STAGED_FILE to APP_DIR
+# UNZIP STAGED_FILE to APP_DIR
+/usr/bin/tar -xvf $STAGED_FILE -C $APP_DIR LCEDA-Pro.app
