0a4b3c4125efe711ff7e3e692c46ce663aa508ce
ivaquero <msaintms@outlook.com>
2024-09-26 23:55:18 +0800

    lceda-pro: fix audit

--- Casks/l/lceda-pro.rb
+++ Casks/l/lceda-pro.rb
@@ -7,7 +7,8 @@ cask "lceda-pro" do
          intel: "0adf3ee96c7777cf72f7db11a77b051888648589f89e4050c32ac4fde50fcf23"
 
   branch = "main"
-  url "https://raw.githubusercontent.com/Brewforge/homebrew-chinese/refs/heads/#{branch}/Casks/l/lceda-pro.sh"
+  url "https://raw.githubusercontent.com/Brewforge/homebrew-chinese/refs/heads/#{branch}/Casks/l/lceda-pro.sh",
+      verified: "raw.githubusercontent.com/Brewforge/homebrew-chinese/"
   name "LCEDA-Pro"
   desc "高效的国产 PCB 设计工具，永久免费"
   homepage "https://lceda.cn/"
@@ -26,7 +27,7 @@ cask "lceda-pro" do
     args:       [
       "https://image.lceda.cn/files/lceda-pro-mac-#{arch}-#{version}.zip",
       "#{staged_path}/lceda-pro-mac-#{arch}-#{version}.zip",
-      "#{appdir}",
+      appdir.to_s,
     ],
   }
 
