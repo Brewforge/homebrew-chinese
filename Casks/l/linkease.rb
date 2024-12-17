648b4a8b055f9cfe2e5c05f6754c4a4f6af408c6
ivaquero <msaintms@outlook.com>
2024-12-17 12:32:04 +0800

    linkease: fix url

--- Casks/l/linkease.rb
+++ Casks/l/linkease.rb
@@ -2,11 +2,11 @@ cask "linkease" do
   version "1.2.3"
   sha256 :no_check
 
-  url "https://fw8.koolcenter.com:5000/binary/LinkEase/Client/LinkEase.dmg",
-      verified: "fw8.koolcenter.com:5000/"
+  url "https://fw.koolcenter.com/binary/LinkEase/Client/LinkEase.dmg",
+      verified: "fw.koolcenter.com/binary/"
   name "LinkEase"
   desc "一个可以随时随地远程连接个人 文件或设备的私有存储云"
-  homepage "https://doc.linkease.com/"
+  homepage "https://www.linkease.com/"
 
   livecheck do
     url "https://doc.linkease.com/zh/guide/linkease/install/device/support.html"
