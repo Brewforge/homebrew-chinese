b3493e9e786d53a268c1dc3395a548aa869d1e6f
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2025-01-15 04:20:18 +0800

    ollamaplist 0.2.2

--- Formula/ollamaplist.rb
+++ Formula/ollamaplist.rb
@@ -1,9 +1,8 @@
 class Ollamaplist < Formula
   desc "给 Ollama CLI 添加环境变量"
   homepage "https://github.com/Mrered/Gobin"
-  url "https://mirror.ghproxy.com/https://github.com/Mrered/Gobin/releases/download/v#{version}/ollamaplist_#{version}_darwin_arm64.tar.gz",
+  url "https://mirror.ghproxy.com/https://github.com/Mrered/Gobin/releases/download/v0.2.2/ollamaplist_0.2.2_darwin_arm64.tar.gz",
     verified: "mirror.ghproxy.com/"
-  version "0.2.2"
   sha256 "9ffa61dfdc430d21c7e675205a3a73768f9a063065e8b8f9a55b3b3b71d24ebf"
   license "MIT"
 
@@ -14,7 +13,7 @@ class Ollamaplist < Formula
 
   on_macos do
     on_intel do
-      url "https://mirror.ghproxy.com/https://github.com/Mrered/Gobin/releases/download/v#{version}/ollamaplist_#{version}_darwin_amd64.tar.gz",
+      url "https://mirror.ghproxy.com/https://github.com/Mrered/Gobin/releases/download/v0.2.2/ollamaplist_0.2.2_darwin_amd64.tar.gz",
         verified: "mirror.ghproxy.com/"
       sha256 "d311991876899604b5000730e21f0c17c38d34fef4a5d2e6f00ade044250d008"
     end
