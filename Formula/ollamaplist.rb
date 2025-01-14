0f7ef4456dcc4c30c1551fc6a2cd53388218486e
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2025-01-15 03:22:53 +0800

    ollamaplist 0.2.2

--- Formula/ollamaplist.rb
+++ Formula/ollamaplist.rb
@@ -4,15 +4,21 @@ class Ollamaplist < Formula
   version "0.2.2"
   license "MIT"
 
-  mirror = "https://mirror.ghproxy.com/"
-  if OS.mac? && Hardware::CPU.arm?
-    url "#{mirror}https://github.com/Mrered/Gobin/releases/download/v#{version}/ollamaplist_#{version}_darwin_arm64.tar.gz",
-      verified: "mirror.ghproxy.com/"
-    sha256 "9ffa61dfdc430d21c7e675205a3a73768f9a063065e8b8f9a55b3b3b71d24ebf"
-  elsif OS.mac? && !Hardware::CPU.arm?
-    url "#{mirror}https://github.com/Mrered/Gobin/releases/download/v#{version}/ollamaplist_#{version}_darwin_amd64.tar.gz",
-      verified: "mirror.ghproxy.com/"
-    sha256 "d311991876899604b5000730e21f0c17c38d34fef4a5d2e6f00ade044250d008"
+  on_macos do
+    on_arm do
+      resource "ollamaplist" do
+        url "https://mirror.ghproxy.com/https://github.com/Mrered/Gobin/releases/download/v#{version}/ollamaplist_#{version}_darwin_arm64.tar.gz",
+          verified: "mirror.ghproxy.com/"
+        sha256 "9ffa61dfdc430d21c7e675205a3a73768f9a063065e8b8f9a55b3b3b71d24ebf"
+      end
+    end
+    on_intel do
+      resource "ollamaplist" do
+        url "https://mirror.ghproxy.com/https://github.com/Mrered/Gobin/releases/download/v#{version}/ollamaplist_#{version}_darwin_amd64.tar.gz",
+          verified: "mirror.ghproxy.com/"
+        sha256 "d311991876899604b5000730e21f0c17c38d34fef4a5d2e6f00ade044250d008"
+      end
+    end
   end
 
   livecheck do
@@ -21,7 +27,9 @@ class Ollamaplist < Formula
   end
 
   def install
-    bin.install "ollamaplist"
+    resource("ollamaplist").stage do
+      bin.install "ollamaplist"
+    end
   end
 
   def post_install
