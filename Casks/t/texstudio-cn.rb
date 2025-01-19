b388c871c7e07ba3396a00a8b241a09d5de34a18
ivaquero <msaintms@outlook.com>
2025-01-19 11:33:44 +0800

    texstudio-cn: update to 4.8.5

--- Casks/t/texstudio-cn.rb
+++ Casks/t/texstudio-cn.rb
@@ -1,15 +1,18 @@
 cask "texstudio-cn" do
-  version "4.8.4"
-  sha256 "43f70201a54d3622983093d99636529935a6d911a5cd99f70637c35f052ccd6e"
+  arch arm: "-m1", intel: ""
 
-  url "https://mirrors.bfsu.edu.cn/github-release/texstudio-org/texstudio/LatestRelease/texstudio-#{version}-osx.zip",
-      verified: "mirrors.bfsu.edu.cn/github-release/texstudio-org/"
+  version "4.8.5"
+  sha256 arm:   "dd7b7b2d5918afe10c35ebdb06790b35b72364740faf9e35ef7b78b5dba456fc",
+         intel: "1f1925bda6d82003481c1952f8702597dfb71657d2ad774e452af1e9e45036d4"
+
+  url "https://mirrors.tuna.tsinghua.edu.cn/github-release/texstudio-org/texstudio/LatestRelease/texstudio-#{version}-osx#{arch}.zip",
+      verified: "mirrors.tuna.tsinghua.edu.cn/github-release/texstudio-org/"
   name "TeXstudio"
   desc "LaTeX editor"
   homepage "https://github.com/texstudio-org/texstudio/"
 
   livecheck do
-    url "https://mirrors.bfsu.edu.cn/github-release/texstudio-org/texstudio/LatestRelease"
+    url "https://mirrors.tuna.tsinghua.edu.cn/github-release/texstudio-org/texstudio/LatestRelease"
     regex(/texstudio-(\d+(\.\d+){2})-osx\.zip/i)
   end
 
@@ -17,15 +20,12 @@ cask "texstudio-cn" do
   conflicts_with cask: "texstudio"
   depends_on macos: ">= :big_sur"
 
-  app "texstudio.app"
+  app "texstudio-#{version}-osx#{arch}.app"
 
   zap trash: [
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/texstudio.sfl*",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/C/texstudio",
+    "~/.config/texstudio",
     "~/Library/Preferences/texstudio.plist",
     "~/Library/Saved Application State/texstudio.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
