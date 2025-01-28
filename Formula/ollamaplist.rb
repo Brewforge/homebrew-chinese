4f400273ab1fe1771bfee3d435c24fe821cf2012
ivaquero <msaintms@outlook.com>
2025-01-29 00:35:55 +0800

    ollamaplist 0.2.3

--- Formula/ollamaplist.rb
+++ Formula/ollamaplist.rb
@@ -1,17 +1,14 @@
 class Ollamaplist < Formula
   desc "给 Ollama CLI 添加环境变量"
   homepage "https://github.com/Mrered/Gobin"
-  version "0.2.2"
+  version "0.2.3"
   license "MIT"
 
-  mirror = "https://mirror.ghproxy.com/"
   if OS.mac? && Hardware::CPU.arm?
-    url "#{mirror}https://github.com/Mrered/Gobin/releases/download/v#{version}/ollamaplist_#{version}_darwin_arm64.tar.gz",
-      verified: "mirror.ghproxy.com/"
-    sha256 "9ffa61dfdc430d21c7e675205a3a73768f9a063065e8b8f9a55b3b3b71d24ebf"
+    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/ollamaplist_0.2.3_darwin_arm64.tar.gz"
+    sha256 "b36aa988ea59cf11b9fd3954e4b3720dd9b8d225099253466e932ce7f81a9dd9"
   elsif OS.mac? && !Hardware::CPU.arm?
-    url "#{mirror}https://github.com/Mrered/Gobin/releases/download/v#{version}/ollamaplist_#{version}_darwin_amd64.tar.gz",
-      verified: "mirror.ghproxy.com/"
+    url "https://github.com/Mrered/Gobin/releases/download/v0.2.2/ollamaplist_0.2.2_darwin_amd64.tar.gz"
     sha256 "d311991876899604b5000730e21f0c17c38d34fef4a5d2e6f00ade044250d008"
   end
 
@@ -26,5 +23,9 @@ class Ollamaplist < Formula
 
   def post_install
     chmod 0555, bin/"ollamaplist"
+  end
+
+  test do
+    system bin/"ollamaplist", "-h"
   end
 end
