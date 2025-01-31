30a884a1c26a4667d72b3e2a6b374ba84a4db7c4
ivaquero <msaintms@outlook.com>
2025-01-31 10:29:20 +0800

    makemf: update to 0.2.4

--- Formula/makemf.rb
+++ Formula/makemf.rb
@@ -9,13 +9,13 @@ class Makemf < Formula
     sha256 "c09a6bc634e3b90f6f52e3b6711aa27fc8aec3cf7ae59acc897ae9a965be32f6"
   elsif OS.mac? && !Hardware::CPU.arm?
     url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/makemf_0.2.3_darwin_amd64.tar.gz"
-    sha256 "a9a4a1e0ae92d996a7db3da6d4bb84bce689963b1d1083e0f26569ad939013d9"
+    sha256 "2f6f5312c85b832b9c4397c779cd1168547afb99e0dc6226ab933512bcc6ec53"
   elsif !OS.mac? && Hardware::CPU.arm?
     url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/makemf_0.2.3_linux_arm64.tar.gz"
-    sha256 "e75a5fa805f2cd383f7f1ae0a6f8065eb38fdf41d59607d44abd675a9419a23d"
+    sha256 "6a00351436126ca5b0fde825ab3b90ef2edb1345c5f8d3226512b09756f627d0"
   elsif !OS.mac? && !Hardware::CPU.arm?
     url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/makemf_0.2.3_linux_amd64.tar.gz"
-    sha256 "56a40362b8c8b5b47238932a76dc880d3a7a749c0b1c6626f66549dd5a27381f"
+    sha256 "0020268aff143c9d4fa69b595f57344674791f902f4ea5efad0bb3c607e25aca"
   end
 
   livecheck do
--- Formula/ollamaplist.rb
+++ Formula/ollamaplist.rb
@@ -5,10 +5,10 @@ class Ollamaplist < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/ollamaplist_#{version}_darwin_arm64.tar.gz"
+    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/ollamaplist_0.2.3_darwin_arm64.tar.gz"
     sha256 "b36aa988ea59cf11b9fd3954e4b3720dd9b8d225099253466e932ce7f81a9dd9"
   elsif OS.mac? && !Hardware::CPU.arm?
-    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/ollamaplist_#{version}_darwin_amd64.tar.gz"
+    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/ollamaplist_0.2.3_darwin_amd64.tar.gz"
     sha256 "7610f44bd80659d3fb6c8a05cd51f399a91607e7bb5bfebdde4b52599b2ba2f9"
   end
 
