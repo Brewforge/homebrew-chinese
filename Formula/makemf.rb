9ad1388f99e08f7d18f517f9e8bb37134543db15
_ivaquero_ <46377665+ivaquero@users.noreply.github.com>
2025-01-31 10:27:31 +0800

    makemf 0.2.4 (#572)

--- Formula/makemf.rb
+++ Formula/makemf.rb
@@ -1,20 +1,20 @@
 class Makemf < Formula
   desc "让 GGUF 在 Ollama 中运行"
   homepage "https://github.com/Mrered/Gobin"
-  version "0.2.3"
+  version "0.2.4"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/makemf_#{version}_darwin_arm64.tar.gz"
-    sha256 "d1a4fdd94b56253f76d572cede00dba7e17ebe4bb6d4e0bd2b2d28baa2cda20f"
+    url "https://github.com/Mrered/Gobin/releases/download/v0.2.4/makemf_0.2.4_darwin_arm64.tar.gz"
+    sha256 "c09a6bc634e3b90f6f52e3b6711aa27fc8aec3cf7ae59acc897ae9a965be32f6"
   elsif OS.mac? && !Hardware::CPU.arm?
-    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/makemf_#{version}_darwin_amd64.tar.gz"
+    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/makemf_0.2.3_darwin_amd64.tar.gz"
     sha256 "a9a4a1e0ae92d996a7db3da6d4bb84bce689963b1d1083e0f26569ad939013d9"
   elsif !OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/makemf_#{version}_linux_arm64.tar.gz"
+    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/makemf_0.2.3_linux_arm64.tar.gz"
     sha256 "e75a5fa805f2cd383f7f1ae0a6f8065eb38fdf41d59607d44abd675a9419a23d"
   elsif !OS.mac? && !Hardware::CPU.arm?
-    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/makemf_#{version}_linux_amd64.tar.gz"
+    url "https://github.com/Mrered/Gobin/releases/download/v0.2.3/makemf_0.2.3_linux_amd64.tar.gz"
     sha256 "56a40362b8c8b5b47238932a76dc880d3a7a749c0b1c6626f66549dd5a27381f"
   end
 
