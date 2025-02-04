a65cd290b4d206c0fc04b4f24b650c52c9bce320
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2025-02-04 21:37:04 +0800

    makemf: 0.3.0 ollamaplist: 0.3.0 reportgen: 0.3.0

--- Formula/ollamaplist.rb
+++ Formula/ollamaplist.rb
@@ -1,31 +1,17 @@
 class Ollamaplist < Formula
   desc "给 Ollama CLI 添加环境变量"
   homepage "https://github.com/Mrered/Gobin"
-  version "0.2.4"
+  url "https://github.com/Mrered/Gobin/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "52d807b878a4259b68e4a4f8c5980c0c6c9036349c0b63b6c7144a1330539b09"
   license "MIT"
+  head "https://github.com/Mrered/Gobin.git", branch: "main"
 
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Mrered/Gobin/releases/download/v0.2.4/ollamaplist_0.2.4_darwin_arm64.tar.gz"
-    sha256 "f4275df70abefb206f3f2ad756e5420a6d8870d4d9b4b0b347769bd76f223d57"
-  elsif OS.mac? && !Hardware::CPU.arm?
-    url "https://github.com/Mrered/Gobin/releases/download/v0.2.4/ollamaplist_0.2.4_darwin_amd64.tar.gz"
-    sha256 "b8bc2b6b2c936680d2efd381897d2eb1285a99c3c6a95ad5fd353c0aed2120bd"
-  end
-
-  livecheck do
-    url :homepage
-    strategy :github_latest
-  end
-
+  depends_on "go" => :build
   def install
-    bin.install "ollamaplist"
-  end
-
-  def post_install
-    chmod 0555, bin/"ollamaplist"
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/ollamaplist"
   end
 
   test do
-    system bin/"ollamaplist", "-h"
+    system bin/"ollamaplist", "-v"
   end
 end
