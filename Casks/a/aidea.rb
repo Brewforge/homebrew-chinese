369b299bc8b8fab0872907e2021250136656dc6e
ivaquero <msaintms@outlook.com>
2025-02-05 14:59:36 +0800

    aidea: update zap

--- Casks/a/aidea.rb
+++ Casks/a/aidea.rb
@@ -2,8 +2,8 @@ cask "aidea" do
   version "1.0.15"
   sha256 "61b2e3cacdf08d09c7aa3726e6b0fc14c752c05dec4e7afdeb237f3c5b39f55a"
 
-  url "https://github.com/mylxsw/aidea/releases/download/#{version}/aidea-#{version}-macos.dmg"
-
+  url "https://github.com/mylxsw/aidea/releases/download/#{version}/aidea-#{version}-macos.dmg",
+      verified: "github.com/mylxsw/aidea/"
   name "AIdea"
   desc "一款支持 GPT 以及国产大语言模型，以及 Stable Diffusion 的全能型 APP"
   homepage "https://ai.aicode.cc/"
@@ -15,5 +15,11 @@ cask "aidea" do
 
   app "aidea.app"
 
-  zap trash: []
+  zap trash: [
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/C/cc.aicode.flutter.askaide.askaide",
+    "~/.aidea",
+    "~/Library/Application Support/cc.aicode.flutter.askaide.askaide",
+    "~/Library/Caches/cc.aicode.flutter.askaide.askaide",
+    "~/Library/Saved Application State/cc.aicode.flutter.askaide.askaide.savedState",
+  ]
 end
--- Casks/e/easytier.rb
+++ Casks/e/easytier.rb
@@ -5,7 +5,8 @@ cask "easytier" do
   sha256 arm:   "0df9499d65eb65a11f4728e726955d2781fe3ec02e45821eee8cc36a47b20960",
          intel: "7e12653ab12631051ca559ee55c2bc80f8ead83559203ed1613cb0bdaa76b8c8"
 
-  url "https://github.com/EasyTier/EasyTier/releases/download/v#{version}/easytier-gui_#{version}_#{arch}.dmg"
+  url "https://github.com/EasyTier/EasyTier/releases/download/v#{version}/easytier-gui_#{version}_#{arch}.dmg",
+    verified: "github.com/EasyTier/EasyTier/"
 
   name "EasyTier"
   desc "简单、安全、去中心化的内网穿透 VPN 组网方案"
