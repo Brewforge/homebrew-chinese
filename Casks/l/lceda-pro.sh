d0468bd204fef2c07a5ae5a389af0def3b58ff7c
ivaquero <msaintms@outlook.com>
2025-02-05 16:15:00 +0800

    lceda-pro: 💄 fix style

--- Casks/l/lceda-pro.sh
+++ Casks/l/lceda-pro.sh
@@ -5,7 +5,7 @@ STAGED_FILE=$2
 APP_DIR=$3
 
 # download lceda-pro from URL to STAGED_FILE, show progress
-curl -L -o "$STAGED_FILE" "$URL"
+curl -L -o "${STAGED_FILE}" "${URL}"
 
 # UNZIP STAGED_FILE to APP_DIR
-/usr/bin/tar -xvf "$STAGED_FILE" -C "$APP_DIR" LCEDA-Pro.app
+/usr/bin/tar -xvf "${STAGED_FILE}" -C "${APP_DIR}" LCEDA-Pro.app
