a0351ddd4c6fcc0d37bea2335c3368e1f37d2a08
ivaquero <msaintms@outlook.com>
2025-03-19 18:33:51 +0800

    splayer-imsyy: add zap

--- Casks/s/splayer-imsyy.rb
+++ Casks/s/splayer-imsyy.rb
@@ -1,15 +1,18 @@
 cask "splayer-imsyy" do
-  version "3.0.0-beta.1"
-  sha256 "ccbc6a30c046f0a3e9efb861d435b218def68971b65891ee7eede17eae3f1f6c"
+  arch arm: "-arm64", intel: ""
 
-  url "https://github.com/imsyy/SPlayer/releases/download/v#{version}/splayer-#{version}.dmg"
+  version "3.0.0-beta.1"
+  sha256 arm:   "51a54652a76ed145133ac54544f08bff5590d99c7fd31055161c01131c323367",
+         intel: "22479889d413a70c5f5372dfca46f134a5f5216255a667fa8457d2190fe1ad42"
+
+  url "https://github.com/imsyy/SPlayer/releases/download/v#{version}/splayer-#{version}-mac.zip"
   name "SPlayer"
   desc "简约的音乐播放器"
   homepage "https://github.com/imsyy/SPlayer"
 
   livecheck do
     url "https://github.com/imsyy/SPlayer/releases"
-    regex(/splayer-v?(\d+(?:\.\d+)+)(-beta\.\d)?\.dmg$/i)
+    regex(/splayer-v?(\d+(?:\.\d+)+)(-beta\.\d)?#{arch}-mac\.zip$/i)
     strategy :github_latest do |json, regex|
       json["assets"]&.map do |asset|
         match = asset["browser_download_url"]&.match(regex)
@@ -24,5 +27,31 @@ cask "splayer-imsyy" do
 
   app "SPlayer.app"
 
-  zap trash: []
+  zap trash: [
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/C/com.imsyy.splayer",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/C/com.imsyy.splayer.helper.GPU",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.AqfEFU",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.BzfhiL",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.DWAwjb",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.GZPyQh",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.hfIAKT",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.HHoeBW",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.hypzun",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.IEZz3f",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.KLAtwS",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.KlbVAX",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.M3pE2r",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.MRG9Ne",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.S1m6Ay",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.Sjp5VC",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.tmFZvk",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.v3M8rZ",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.VbqLvd",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.WqeSwl",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/.com.imsyy.splayer.YctRpg",
+    "~/Documents/SPlayer",
+    "~/Library/Application Support/SPlayer",
+    "~/Library/Preferences/com.imsyy.splayer.plist",
+    "~/Library/Saved Application State/com.imsyy.splayer.savedState",
+  ]
 end
