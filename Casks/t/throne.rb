079c1995039b47acb72889e08fdda11b71b954b8
ivaquero <msaintms@outlook.com>
2025-08-02 16:36:08 +0800

    throne: skip pre-release

--- Casks/t/throne.rb
+++ Casks/t/throne.rb
@@ -1,9 +1,9 @@
 cask "throne" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.0.0-beta.6"
-  sha256 arm:   "0bfd63c854297b8f38253ef66ab78ae210a82fae8f3a6c5bad4a5f210e57f6ff",
-         intel: "f8dbc750efffadb2e0d222e4f6ac74262e62679ee113aac52870259e7e894cc6"
+  version "1.0.0-beta.7"
+  sha256 arm:   "7bb6fafffd311006fb15c7420446df09cf5442408c4a4ac1cef475a4c80faa7c",
+         intel: "6caedf4762932bca08c046e20791830bf135ee6e2280ccbd68c9888d00c0e5f7"
 
   url "https://github.com/throneproj/Throne/releases/download/#{version}/Throne-#{version}-macos-#{arch}.zip"
   name "Throne"
@@ -11,13 +11,14 @@ cask "throne" do
   homepage "https://github.com/throneproj/Throne"
 
   livecheck do
-    url "https://github.com/throneproj/Throne/tags"
-    strategy :page_match do |page|
-      match = page.match(/href=.*?(\d+(\.\d+)+)(-beta\.\d)/i)
-      next if match.blank?
+    # url "https://github.com/throneproj/Throne/tags"
+    # strategy :page_match do |page|
+    #   match = page.match(/href=.*?(\d+(\.\d+)+)(-beta\.\d)/i)
+    #   next if match.blank?
 
-      match[1].to_s + match[3].to_s
-    end
+    #   match[1].to_s + match[3].to_s
+    # end
+    skip "No livecheck as pre-release"
   end
 
   auto_updates true
