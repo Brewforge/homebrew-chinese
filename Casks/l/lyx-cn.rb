970a31cbed6b4e71fe63b6226798e9c697f4083e
ivaquero <msaintms@outlook.com>
2025-08-03 23:22:41 +0800

    lyx-cn: fix livecheck

--- Casks/l/lyx-cn.rb
+++ Casks/l/lyx-cn.rb
@@ -10,7 +10,7 @@ cask "lyx-cn" do
 
   livecheck do
     url "https://lyx.org/Download"
-    regex(/LyX-v?(\d+(?:\.\d+)+)\+qt5/i)
+    regex(/LyX[._-]v?(\d+(?:\.\d+)+)\+qt5/i)
   end
 
   auto_updates true
