d0eb8b43f64a9a670e3c4d0521b17ca1deefd3fc
ivaquero <msaintms@outlook.com>
2025-11-19 00:59:06 +0800

    blender-cn: fix livecheck

--- Casks/b/blender-cn.rb
+++ Casks/b/blender-cn.rb
@@ -1,9 +1,12 @@
 cask "blender-cn" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.5.3"
-  sha256 arm:   "73ea841053b55404bb3a71a9a22366f1f8821787fe5c899f8b55a7fff929d01b",
-         intel: "c1fd8f30eef1b37918659ad65c504d30cebd3e5bac74dd54f7df1a311aeebe18"
+  # NOTE: Please manually check for a new `blender@lts` version at
+  # https://www.blender.org/download/lts/ when updating this cask, as we cannot
+  # identify LTS versions using livecheck.
+  version "4.5.5"
+  sha256 arm:   "de1d239d1326378744326bdf1a55f935cdd3d7e808eadd0d601bba28e06e05f6",
+         intel: "f60c27f9a52ccde87e9e68ae8184e5c8c56f8b622d0657b345c64ad2b1232174"
 
   url "https://mirror.nju.edu.cn/blender/release/Blender#{version.major_minor}/blender-#{version}-macos-#{arch}.dmg",
       verified: "mirror.nju.edu.cn/"
@@ -11,9 +14,29 @@ cask "blender-cn" do
   desc "3D creation suite"
   homepage "https://blender.org/"
 
+  # The upstream download page (https://www.blender.org/download/) cannot be
+  # fetched due to Cloudflare protections, so we have to naively assume a
+  # version is released when the assets are uploaded.
   livecheck do
-    url "https://www.blender.org/download/"
-    regex(%r{href=.*?/blender[._-]v?(\d+(?:\.\d+)+)[._-]macos[._-]#{arch}\.dmg}i)
+    url "https://download.blender.org/release/"
+    regex(/href=.*?blender[._-]v?(\d+(?:\.\d+)+)(?:[._-]macos)?[._-]#{arch}\.dmg/i)
+    strategy :page_match do |page, regex|
+      # Match major/minor versions from stable directory names
+      major_minor_versions =
+        page.scan(%r{href=["']?[^"' >]*?Blender[._-]?v?(\d+(?:\.\d+)+)/?["' >]}i)
+            .flatten
+            .uniq
+            .sort_by { |v| Version.new(v) }
+      next if major_minor_versions.blank?
+
+      # Check the directory listing page for the highest major/minor version
+      directory_page = Homebrew::Livecheck::Strategy.page_content(
+        URI.join(@url, "Blender#{major_minor_versions.last}/").to_s,
+      )[:content]
+      next if directory_page.blank?
+
+      directory_page.scan(regex).map { |match| match[0] }
+    end
   end
 
   auto_updates true
