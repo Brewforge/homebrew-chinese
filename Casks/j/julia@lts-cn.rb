1418e4200f621ce54fa6f15e96206b4025544e5b
ivaquero <msaintms@outlook.com>
2026-01-08 12:48:07 +0800

    fix mirrors

--- Casks/j/julia@lts-cn.rb
+++ Casks/j/julia@lts-cn.rb
@@ -4,8 +4,8 @@ cask "julia@lts-cn" do
   version "1.10.10"
   sha256 "297ff71261e9e77114601c3b1ab1a3056621911887e791192e219558f35939a6"
 
-  url "https://mirrors.ustc.edu.cn/julia-releases/bin/mac/aarch64/#{version.major_minor}/julia-#{version}-macaarch64.dmg",
-      verified: "mirrors.ustc.edu.cn/julia-releases/bin/mac/"
+  url "https://mirrors.tuna.tsinghua.edu.cn/julia-releases/bin/mac/#{arch}/#{version.major_minor}/julia-#{version}-macaarch64.dmg",
+      verified: "mirrors.tuna.tsinghua.edu.cn/julia-releases/bin/mac/"
   name "Julia"
   desc "Programming language for technical computing"
   homepage "https://julialang.org/"
