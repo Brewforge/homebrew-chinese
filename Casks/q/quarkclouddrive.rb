1ff391ebddaad196651f28db3ba830675f9b0958
ivaquero <msaintms@outlook.com>
2026-04-15 20:57:14 +0800

    quarkclouddrive: fix livecheck

--- Casks/q/quarkclouddrive.rb
+++ Casks/q/quarkclouddrive.rb
@@ -1,8 +1,8 @@
 cask "quarkclouddrive" do
-  version "3.24.0,2830624"
+  version "3.24.0,2830624,37213"
   sha256 "b2fb15cedb2fa35b2b9e850ffe6cb17724b6cb71d34b1422ea1aa27e93bef2ad"
 
-  url "https://pdds.quark.cn/download/stfile/#{version.csv.third}/QuarkCloudDrive_v#{version.csv.first}_release_(Build#{version.csv.second}).dmg"
+  url "https://umcdn.quark.cn/download/37213/quarkclouddrivemac/pckk@product_guanwang/QuarkCloudDriveMac_V#{version.csv.first}_mac_pf30003_(zh-cn)_release_(Build#{version.csv.second}).dmg"
   name "夸克网盘"
   desc "电脑高效拍档，一键存爽快看"
   homepage "https://pan.quark.cn/"
@@ -11,10 +11,10 @@ cask "quarkclouddrive" do
     url "https://pan.quark.cn/api/client_version"
     strategy :page_match do |page|
       data = JSON.parse(page)["data"]["origin_macDmgForArmUrl"]
-      match = data.match(/QuarkCloudDriveMac_V(\d+(\.\d+)+)_mac_pf30003_\(zh-cn\)_release_\(Build(\d{7})\)\.dmg/i)
+      match = data.match(%r{/(\d{5})/quarkclouddrivemac/pckk@product_guanwang/QuarkCloudDriveMac_V(\d+(\.\d+)+)_mac_pf30003_\(zh-cn\)_release_\(Build(\d{7})\)\.dmg}i)
       next if match.blank?
 
-      "#{match[1]},#{match[3]}"
+      "#{match[2]},#{match[4]},#{match[1]}"
     end
   end
 
