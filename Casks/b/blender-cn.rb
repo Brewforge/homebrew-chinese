667e2f273ee0ab399c0e64183b189f80ef1f7acc
ivaquero <msaintms@outlook.com>
2026-04-24 17:51:10 +0800

    blender-cn: update to 5.1.1

--- Casks/b/blender-cn.rb
+++ Casks/b/blender-cn.rb
@@ -1,6 +1,6 @@
 cask "blender-cn" do
-  version "5.1.0"
-  sha256 "ebeda0dbb7fbf06f3cbcf58c11397551de745f1177acfe3e2331ac0d4d901996"
+  version "5.1.1"
+  sha256 "ff6219b3aaab4d9adf548b9a32b3b31764fe740b6c741d16660c5c0f4ffe9841"
 
   url "https://mirrors.tuna.tsinghua.edu.cn/blender/release/Blender#{version.major_minor}/blender-#{version}-macos-arm64.dmg",
       verified: "mirrors.tuna.tsinghua.edu.cn/"
@@ -9,7 +9,25 @@ cask "blender-cn" do
   homepage "https://www.blender.org/"
 
   livecheck do
-    skip "Cannot be fetched due to Cloudflare protections"
+    url "https://download.blender.org/release/"
+    regex(/href=.*?blender[._-]v?(\d+(?:\.\d+)+)-macos-arm64\.dmg/i)
+    strategy :page_match do |page, regex|
+      # Match major/minor versions from stable directory names
+      major_minor_versions =
+        page.scan(%r{href=["']?[^"' >]*?Blender[._-]?v?(\d+(?:\.\d+)+)/?["' >]}i)
+            .flatten
+            .uniq
+            .sort_by { |v| Version.new(v) }
+      next if major_minor_versions.blank?
+
+      # Check the directory listing page for the highest major/minor version
+      directory_page = Homebrew::Livecheck::Strategy.page_content(
+        URI.join(@url, "Blender#{major_minor_versions.last}/").to_s,
+      )[:content]
+      next if directory_page.blank?
+
+      directory_page.scan(regex).map { |match| match[0] }
+    end
   end
 
   auto_updates true
