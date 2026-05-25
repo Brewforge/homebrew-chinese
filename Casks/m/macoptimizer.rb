6dfc1604348952cb98ea2df7b34e63ff04dbbd72
ivaquero <msaintms@outlook.com>
2026-05-25 16:26:42 +0800

    macoptimizer: fix url

--- Casks/m/macoptimizer.rb
+++ Casks/m/macoptimizer.rb
@@ -5,7 +5,7 @@ cask "macoptimizer" do
   sha256 arm:   "e915ef32f8f87114038c8ea6757906fd80ed3afc338fd5dacab97f45c53f6eb8",
          intel: "fd153c4e20021118704be38a4109940ad8c49b72d8a436d99adfb662fc6bb991"
 
-  url "https://github.com/ddlmanus/MacOptimizer/releases/download/v#{version}/Mac._v#{version}_#{arch}.dmg"
+  url "https://github.com/ddlmanus/MacOptimizer/releases/download/v#{version}/MacOptimizer-v#{version}-#{arch}.dmg"
   name "Mac优化大师"
   desc "系统清理和优化工具"
   homepage "https://github.com/ddlmanus/MacOptimizer"
