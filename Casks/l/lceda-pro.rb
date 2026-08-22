c9034daf30e628528da525746802c7944c3ed0fe
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-08-22 17:58:50 +0800

    lceda-pro: fix

--- Casks/l/lceda-pro.rb
+++ Casks/l/lceda-pro.rb
@@ -2,7 +2,7 @@ cask "lceda-pro" do
   version "3.2.186"
   sha256 "23197786d12eaaea990171aa4967fb4851de546db46c3b43bb6e88baacf6afea"
 
-  url "https://image.lceda.cn/files/lceda-pro-mac-#{arch}-#{version}.zip"
+  url "https://image.lceda.cn/files/lceda-pro-mac-arm64-#{version}.zip"
   name "LCEDA-Pro"
   desc "高效的国产 PCB 设计工具，永久免费"
   homepage "https://lceda.cn/"
