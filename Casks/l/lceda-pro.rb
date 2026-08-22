b2ca8641f0f6f1a8838a27e8d4f9c71bd7797ded
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-08-22 17:17:55 +0800

    lceda-pro: try to fix

--- Casks/l/lceda-pro.rb
+++ Casks/l/lceda-pro.rb
@@ -1,9 +1,6 @@
 cask "lceda-pro" do
-  arch arm: "arm64", intel: "x64"
-
   version "3.2.186"
-  sha256 arm:   "23197786d12eaaea990171aa4967fb4851de546db46c3b43bb6e88baacf6afea",
-         intel: "2d5a6b46bab2773fbc29b00779b92d76e8479077e7c1f5b3f5bc6abbd2e206ad"
+  sha256 "23197786d12eaaea990171aa4967fb4851de546db46c3b43bb6e88baacf6afea"
 
   url "https://image.lceda.cn/files/lceda-pro-mac-#{arch}-#{version}.zip"
   name "LCEDA-Pro"
@@ -12,7 +9,7 @@ cask "lceda-pro" do
 
   livecheck do
     url "https://lceda.cn/page/download"
-    regex(/lceda-pro-mac-#{arch}-(\d+(\.\d+)+)/i)
+    regex(/lceda-pro-mac-arm64-(\d+(\.\d+)+)/i)
   end
 
   depends_on :macos
