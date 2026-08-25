1bf9c4926158480142f8dbb19402806e662a6350
Oscaner Miao <oscaner1997@gmail.com>
2026-08-25 18:59:12 +0800

    anymo 2.7.11

---
+++ Casks/a/anymo.rb
@@ -0,0 +1,31 @@
+cask "anymo" do
+  arch arm: "arm64", intel: "x64"
+
+  version "2.7.11"
+  sha256 :no_check
+
+  url "https://anymoweb.com/app-mac-#{arch}.dmg",
+      verified: "anymoweb.com/"
+  name "Anymo"
+  desc "终身受用的抗遗忘卡片记忆笔记"
+  homepage "https://anymoweb.com/"
+
+  livecheck do
+    url "https://anymoweb.com/app_client/latest.yml"
+    regex(/version:\s*"?(\d+(?:\.\d+)+)"?/i)
+  end
+
+  depends_on macos: :catalina
+
+  app "Anymo.app"
+
+  preflight do
+    system_command "xattr",
+                   args: ["-cr", "#{staged_path}/Anymo.app"]
+  end
+
+  zap trash: [
+    "~/Library/Application Support/Anymo",
+    "~/Library/Preferences/com.memo93.anymodesktop.plist",
+  ]
+end
